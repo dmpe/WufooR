7999db4f792ed4f436d6d497f2aaf4c55f68c097
John Malc <cincenko@outlook.com>
2019-03-23 10:30:19 +0100

    still 421 limit exceeded

--- vignettes/Tutorial.R
+++ vignettes/Tutorial.R
@@ -1,49 +1,9 @@
-## ----echo=FALSE----------------------------------------------------------
+## ----echo=TRUE-----------------------------------------------------------
 library(WufooR)
 # put here your api key
 options(Wufoo_Name = "johnmalc", Wufoo_API = "S6VI-I8UA-BY11-TDHO")
 
-## ---- eval=FALSE, include=T----------------------------------------------
-#  auth_name(NULL)
-#  auth_key(NULL)
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  t(user_info())
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  t(form_info(formIdentifier = "z5kqx7h1gtvg4g", includeTodayCount="FALSE"))
-#  
-#  
-#  t(form_info(includeTodayCount="FALSE"))
-#  
-#  # Show responses to the form
-#  fe_1 <- form_entries(formIdentifier = "z5kqx7h1gtvg4g")
-#  t(fe_1)
-#  
-#  sapply(fe_1, class)
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  form_entriesCount(formIdentifier = "z5kqx7h1gtvg4g", domain = "wufoo.eu")
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  form_comments(formIdentifier = "z5kqx7h1gtvg4g", domain = "wufoo.eu")
-#  form_commentsCount(formIdentifier = "z5kqx7h1gtvg4g")
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  form_entriesFromCSV(reportName = "untitled-report", showRequestURL = F)
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  fields_info(formIdentifier = "z5kqx7h1gtvg4g", showRequestURL = FALSE)
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  # all reports
-#  t(reports_info())
-#  
-#  # only some reports
-#  t(report_info(reportName = "untitled-report"))
-
-## ---- eval=FALSE, include=T----------------------------------------------
-#  t(report_entries(reportName = "untitled-report"))
-#  
-#  t(report_widgets(formIdentifier = "z5kqx7h1gtvg4g"))
+## ---- eval=TRUE, include=T-----------------------------------------------
+auth_name(NULL)
+auth_key(NULL)
 
